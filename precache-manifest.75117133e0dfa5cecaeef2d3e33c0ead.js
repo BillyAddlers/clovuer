9655b7b848cf5a57e2c5842d80849f7ebfdbb84c
BillyAddlers <finnsonalca123@gmail.com>
2020-05-17 09:16:31 +0700

    deploy

---
+++ precache-manifest.75117133e0dfa5cecaeef2d3e33c0ead.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6e197c5f565a6539d1",
+    "url": "/clovuer/css/app.92193ae0.css"
+  },
+  {
+    "revision": "6e72c9a5d49a15d8875f",
+    "url": "/clovuer/css/chunk-vendors.3a61cd70.css"
+  },
+  {
+    "revision": "40430dc50b3cf2a2da6c8cf468dbe784",
+    "url": "/clovuer/img/heart.40430dc5.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/clovuer/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "875770eb4cb4d14142481cf0a6da5318",
+    "url": "/clovuer/index.html"
+  },
+  {
+    "revision": "783508d27ea15f394a1a",
+    "url": "/clovuer/js/about.fba7d292.js"
+  },
+  {
+    "revision": "9e6e197c5f565a6539d1",
+    "url": "/clovuer/js/app.c1baadb1.js"
+  },
+  {
+    "revision": "6e72c9a5d49a15d8875f",
+    "url": "/clovuer/js/chunk-vendors.e350d863.js"
+  },
+  {
+    "revision": "ee09980173006fe69d3e65a3ef9dfd4f",
+    "url": "/clovuer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/clovuer/robots.txt"
+  }
+]);
